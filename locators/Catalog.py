617c86137aaf8b9c9cdd5420c6a4d4358f364f73
Aleksandr Maslov <a.mslv@outlook.com>
2020-08-24 20:19:26 +0300

    added Catalog locators

--- locators/Catalog.py
+++ locators/Catalog.py
@@ -13,5 +13,22 @@ class Catalog:
     class Navigation:
         list_view = {'css': '[data-original-title="List"]'}
         grid_view = {'css': '[data-original-title="Grid"]'}
-        sort_options = {'css': '#input-sort'}
-        limit_options = {'css': '#input-limit'}
+
+    class Sort:
+        options = {'css': '#input-sort'}
+        by_name_A_Z = "Name (A - Z)"
+        by_name_Z_A = "Name (Z - A)"
+        by_price_low_high = "Price (Low > High)"
+        by_price_high_low = "Price (High > Low)"
+        by_raiting_highest = "Rating (Highest)"
+        by_raiting_lowest = "Rating (Lowest)"
+        by_model_A_Z = "Model (A - Z)"
+        by_model_Z_A = "Model (Z - A)"
+        
+    class Limits:
+        options = {'css': '#input-limit'}
+        limit_15 = "15"
+        limit_25 = "25"
+        limit_50 = "50"
+        limit_75 = "70"
+        limit100 = "100"
